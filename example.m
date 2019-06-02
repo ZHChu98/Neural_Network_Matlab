433cf4a3d5bf09a692c871675fedf404a4bed41c
ZHChu98 <845655005@qq.com>
2019-06-02 19:59:12 +0800

    new version

--- example.m
+++ example.m
@@ -10,13 +10,19 @@ labels = gpuArray(labels);
 conv1 = iconv2d(5, 5, 1, 6, 'tanh', 1, 'same');
 y = conv1.forward(images);
 
+% flatten layer
+flatten_layer = iflatten();
+[flatten_layer, y] = flatten_layer.forward(x);
+delta_x = flatten_layer.backward(delta_y)
+
 % dropout layer
-dropout_layer = dropout(0.5);
+dropout_layer = idropout(0.5);
 [dropout_layer, y] = dropout_layer.forward(x);
 delta_x = dropout_layer.backward(delta_y);
 
 % softmax layer
-softmax_layer = softmax();
+softmax_layer = isoftmax();
 y = softmax_layer.forward(x);
 delta_x = softmax_layer.backward(y, labels, 0.01);
 
+delete(p);
