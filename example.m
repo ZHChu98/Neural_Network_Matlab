8a0ebc257a92541b0584877aafeaed59a6c903e4
ZHChu98 <845655005@qq.com>
2019-06-02 18:36:40 +0800

    fixed

--- example.m
+++ example.m
@@ -4,15 +4,19 @@ p = parpool;
 pos = 1;
 [images, labels, pos] = mnist.train.next_batch(64, pos);
 images = gpuArray(images);
+labels = gpuArray(labels);
 
 % convolution layer
 conv1 = iconv2d(5, 5, 1, 6, 'tanh', 1, 'same');
 y = conv1.forward(images);
 
 % dropout layer
-drop1 = dropout(0.5);
+dropout_layer = dropout(0.5);
+[dropout_layer, y] = dropout_layer.forward(x);
+delta_x = dropout_layer.backward(delta_y);
 
-[drop1, y] = drop1.forward(x);
-delta_x = drop1.backward(delta_y);
+% softmax layer
+softmax_layer = softmax();
+y = softmax_layer.forward(x);
+delta_x = softmax_layer.backward(y, labels, 0.01);
 
-disp(size(y));
